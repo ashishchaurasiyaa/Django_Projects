2e052c1c288526f50d85e002bf19fde42d748878
Ashish <AChaurasiya@youngman.co.in>
2023-09-15 14:40:24 +0530

    create Profile view

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,36 +1,35 @@
+# profiles_api/models.py
 from django.db import models
-
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.models import BaseUserManager
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserProfileManager(BaseUserManager):
-    def create_user(self,email,name,password=None):
+    def create_user(self, email, name, password=None):
         if not email:
             raise ValueError('User must have an email address')
 
-        email= self.normalize_email(email)
-        user = self.model(email= email,name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self,email,name,password):
-        user=self.create_user(email,name,password)
-        user.is_superuser= True
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
+
         return user
 
-class UserProfile(AbstractBaseUser,PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
-    object=UserProfileManager()
+    objects = UserProfileManager()  # Note the correct name here
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import viewsets, filters
+from profiles_api import permissions, models
 from rest_framework import status
 from profiles_api import serializers
-from rest_framework import viewsets
 
 class HelloApiView(APIView):
 
-    serializer_class = serializers.HelloSerilizer
+    serializer_class = serializers.HelloSerializer
     """Test API View"""
     def get(self, request,format=None):
         """Returns a list of APIView features"""
@@ -86,3 +88,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
