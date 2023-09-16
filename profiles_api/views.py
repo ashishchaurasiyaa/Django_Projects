ab96ecc9b97c837a6d3c0bc45c96209a3bf46502
Ashish <AChaurasiya@youngman.co.in>
2023-09-16 14:14:37 +0530

    create feed api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,8 @@ from rest_framework import status
 from profiles_api import serializers
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
+
 
 class HelloApiView(APIView):
 
@@ -102,3 +104,12 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user Authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handle creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    def perform_create(self, serializer):
+        """sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
